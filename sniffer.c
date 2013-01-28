0512e962ac1ff59a6397c45e628e2921b42386b2
Vic Hargrave <vichargrave@gmail.com>
2013-01-27 19:16:20 -0800

    Create sniffer.c

---
+++ sniffer.c
@@ -0,0 +1,86 @@
+/*
+   sniffer.c
+
+   Example packet sniffer using the libpcap packet capture library available
+   from http://www.tcpdump.org. Build it on Linux with the following command:
+  
+   ------------------------------------------
+
+   Copyright @ 2012 [Vic Hargrave - http://vichargrave.com]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include <pcap.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ip_icmp.h>
+
+pcap_t* pd;
+int linkhdrlen;
+
+pcap_t* open_pcap_socket(char* device, const char* bpfstr)
+{
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* pd;
+    uint32_t  srcip, netmask;
+    struct bpf_program  bpf;
+
+    // If no network interface (device) is specfied, get the first one.
+    if (!*device && !(device = pcap_lookupdev(errbuf)))
+    {
+        printf("pcap_lookupdev(): %s\n", errbuf);
+        return NULL;
+    }
+    
+    // Open the device for live capture, as opposed to reading a packet
+    // captue file.
+    if ((pd = pcap_open_live(device, BUFSIZ, 1, 0, errbuf)) == NULL)
+    {
+        printf("pcap_open_live(): %s\n", errbuf);
+        return NULL;
+    }
+
+    // Get network device source IP address and netmask.
+    if (pcap_lookupnet(device, &srcip, &netmask, errbuf) < 0)
+    {
+        printf("pcap_lookupnet: %s\n", errbuf);
+        return NULL;
+    }
+
+    // Convert the packet filter epxression into a packet
+    // filter binary.
+    if (pcap_compile(pd, &bpf, (char*)bpfstr, 0, netmask))
+    {
+        printf("pcap_compile(): %s\n", pcap_geterr(pd));
+        return NULL;
+    }
+
+    // Assign the packet filter to the given libpcap socket.
+    if (pcap_setfilter(pd, &bpf) < 0)
+    {
+        printf("pcap_setfilter(): %s\n", pcap_geterr(pd));
+        return NULL;
+    }
+
+    return pd;
+}
