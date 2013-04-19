c4c70d5c707e9ec4784481f2a0996e513969a712
Vic Hargrave <vichargrave@gmail.com>
2013-04-18 17:02:44 -0700

    Updated copyright info

--- sniffer.c
+++ sniffer.c
@@ -6,7 +6,7 @@
   
    ------------------------------------------
 
-   Copyright @ 2012 [Vic Hargrave - http://vichargrave.com]
+   Copyright © 2012 [Vic Hargrave - http://vichargrave.com]
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
