516dcf8f17c3a98845b8046055d02a9216a06a22
Vic Hargrave <vichargrave@gmail.com>
2022-02-13 11:40:17 -0800

    Bug fixes

--- sniffer.c
+++ sniffer.c
@@ -21,83 +21,71 @@
    limitations under the License.
 */
 
-#include <pcap.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netinet/ip.h>
+#include <pcap/pcap.h>
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <netinet/ip_icmp.h>
 
-pcap_t* pd;
+pcap_t* handle;
 int linkhdrlen;
 
-pcap_t* open_pcap_socket(char* device, const char* bpfstr)
+pcap_t* create_pcap_handle(char* device, const char* bpfstr)
 {
     char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t* pd;
-    pcap_if_t* devs = NULL;
-    uint32_t  srcip, netmask;
-    struct bpf_program  bpf;
+    pcap_t *handle = NULL;
+    pcap_if_t* devices = NULL;
+    struct bpf_program bpf;
+    bpf_u_int32 netmask;
+    bpf_u_int32 srcip;
 
     // If no network interface (device) is specfied, get the first one.
-    if (!*device && pcap_findalldevs(&devs, errbuf))
-    {
-        printf("pcap_findalldevs(): %s\n", errbuf);
-        return NULL;
-    }
-    else if (!*device)
-    {
-        device = devs[0].name;
-    }
-    
-    // Open the device for live capture, as opposed to reading a packet
-    // capture file.
-    if ((pd = pcap_open_live(device, BUFSIZ, 1, 0, errbuf)) == NULL)
-    {
-        printf("pcap_open_live(): %s\n", errbuf);
-        return NULL;
+    if (!*device) {
+    	if (pcap_findalldevs(&devices, errbuf)) {
+	        printf("pcap_findalldevs(): %s\n", errbuf);
+	        return NULL;
+	    }
+	    strcpy(device, devices[0].name);
     }
 
     // Get network device source IP address and netmask.
-    if (pcap_lookupnet(device, &srcip, &netmask, errbuf) < 0)
-    {
-        printf("pcap_lookupnet: %s\n", errbuf);
-        return NULL;
+    if (pcap_lookupnet(device, &srcip, &netmask, errbuf) == -1) {
+	    fprintf(stderr, "pcap_lookupnet: %s\n", errbuf);
+	    return NULL;
     }
 
-    // Convert the packet filter epxression into a packet
-    // filter binary.
-    if (pcap_compile(pd, &bpf, (char*)bpfstr, 0, netmask))
-    {
-        printf("pcap_compile(): %s\n", pcap_geterr(pd));
-        return NULL;
+    // Open the device for live capture.
+    handle = pcap_open_live(device, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+	    fprintf(stderr, "pcap_open_live(): %s\n", errbuf);
+	    return NULL;
     }
 
-    // Assign the packet filter to the given libpcap socket.
-    if (pcap_setfilter(pd, &bpf) < 0)
-    {
-        printf("pcap_setfilter(): %s\n", pcap_geterr(pd));
-        return NULL;
+    // Convert the packet filter epxression into a packet filter binary.
+    if (pcap_compile(handle, &bpf, bpfstr, 0, netmask) == -1) {
+	    fprintf(stderr, "pcap_compile(): %s\n", pcap_geterr(handle));
+	    return NULL;
     }
 
-    return pd;
+    // Bind the packet filter to the libpcap handle.    
+    if (pcap_setfilter(handle, &bpf) == -1) {
+	    fprintf(stderr, "pcap_setfilter(): %s\n", pcap_geterr(handle));
+	    return NULL;
+    }
+
+    return handle;
 }
 
-void capture_loop(pcap_t* pd, int packets, pcap_handler func)
+void get_link_header_len(pcap_t* handle)
 {
     int linktype;
  
     // Determine the datalink layer type.
-    if ((linktype = pcap_datalink(pd)) < 0)
-    {
-        printf("pcap_datalink(): %s\n", pcap_geterr(pd));
+    if ((linktype = pcap_datalink(handle)) == -1) {
+        printf("pcap_datalink(): %s\n", pcap_geterr(handle));
         return;
     }
  
@@ -119,22 +107,19 @@ void capture_loop(pcap_t* pd, int packets, pcap_handler func)
  
     default:
         printf("Unsupported datalink (%d)\n", linktype);
-        return;
+        linkhdrlen = 0;
     }
- 
-    // Start capturing packets.
-    if (pcap_loop(pd, packets, func, 0) < 0)
-        printf("pcap_loop failed: %s\n", pcap_geterr(pd));
 }
 
-void parse_packet(u_char *user, struct pcap_pkthdr *packethdr,
-                  u_char *packetptr)
+void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const u_char *packetptr)
 {
     struct ip* iphdr;
     struct icmp* icmphdr;
     struct tcphdr* tcphdr;
     struct udphdr* udphdr;
-    char iphdrInfo[256], srcip[256], dstip[256];
+    char iphdrInfo[256];
+    char srcip[256];
+    char dstip[256];
  
     // Skip the datalink layer header and get the IP header fields.
     packetptr += linkhdrlen;
@@ -164,6 +149,7 @@ void parse_packet(u_char *user, struct pcap_pkthdr *packethdr,
                (tcphdr->th_flags & TH_SYN ? 'F' : '*'),
                ntohl(tcphdr->th_seq), ntohl(tcphdr->th_ack),
                ntohs(tcphdr->th_win), 4*tcphdr->th_off);
+	printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
         break;
  
     case IPPROTO_UDP:
@@ -171,6 +157,7 @@ void parse_packet(u_char *user, struct pcap_pkthdr *packethdr,
         printf("UDP  %s:%d -> %s:%d\n", srcip, ntohs(udphdr->uh_sport),
                dstip, ntohs(udphdr->uh_dport));
         printf("%s\n", iphdrInfo);
+	printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
         break;
  
     case IPPROTO_ICMP:
@@ -179,64 +166,79 @@ void parse_packet(u_char *user, struct pcap_pkthdr *packethdr,
         printf("%s\n", iphdrInfo);
         printf("Type:%d Code:%d ID:%d Seq:%d\n", icmphdr->icmp_type, icmphdr->icmp_code,
                ntohs(icmphdr->icmp_hun.ih_idseq.icd_id), ntohs(icmphdr->icmp_hun.ih_idseq.icd_seq));
+	printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
         break;
     }
-    printf(
-        "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
 }
 
 void bailout(int signo)
 {
     struct pcap_stat stats;
  
-    if (pcap_stats(pd, &stats) >= 0)
+    if (pcap_stats(handle, &stats) >= 0)
     {
         printf("%d packets received\n", stats.ps_recv);
         printf("%d packets dropped\n\n", stats.ps_drop);
     }
-    pcap_close(pd);
+    pcap_close(handle);
     exit(0);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    char interface[256] = "", bpfstr[256] = "";
-    int packets = 0, c, i;
+    char device[256];
+    char bpfstr[256];
+    int count = 0;
+    int opt;
  
+    *device = 0;
+
     // Get the command line options, if any
-    while ((c = getopt (argc, argv, "hi:n:")) != -1)
+    while ((opt = getopt(argc, argv, "hi:n:")) != -1)
     {
-        switch (c)
+        switch (opt)
         {
         case 'h':
-            printf("usage: %s [-h] [-i ] [-n ] []\n", argv[0]);
+            printf("usage: %s [-h] [-i interface] [-n count] [BPF expression]\n", argv[0]);
             exit(0);
             break;
         case 'i':
-            strcpy(interface, optarg);
+            strcpy(device, optarg);
             break;
         case 'n':
-            packets = atoi(optarg);
+            count = atoi(optarg);
             break;
         }
     }
 
     // Get the packet capture filter expression, if any.
-    for (i = optind; i < argc; i++)
+    for (int i = optind; i < argc; i++)
     {
         strcat(bpfstr, argv[i]);
         strcat(bpfstr, " ");
     }
- 
-    // Open libpcap, set the program termination signals then start
-    // processing packets.
-    if ((pd = open_pcap_socket(interface, bpfstr)))
-    {
-        signal(SIGINT, bailout);
-        signal(SIGTERM, bailout);
-        signal(SIGQUIT, bailout);
-        capture_loop(pd, packets, (pcap_handler)parse_packet);
-        bailout(0);
+
+    signal(SIGINT, bailout);
+    signal(SIGTERM, bailout);
+    signal(SIGQUIT, bailout);
+    
+    // Create packet capture handle.
+    handle = create_pcap_handle(device, bpfstr);
+    if (handle == NULL) {
+	    return -1;
     }
-    exit(0);
+
+    // Get the type of link layer.
+    get_link_header_len(handle);
+    if (linkhdrlen == 0) {
+	    return -1;
+    }
+
+    // Start the packet capture with a set count or continually if the count is 0.
+    if (pcap_loop(handle, count, packet_handler, (u_char*)NULL) < 0) {
+    	fprintf(stderr, "pcap_loop failed: %s\n", pcap_geterr(handle));
+	    return -1;
+    }
+    
+    return 0;
 }
